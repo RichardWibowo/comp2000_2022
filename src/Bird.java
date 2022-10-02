99349cc18928d422aec60aab936456e8b1760d53
richardmwibowo <63839269+RichardWibowo@users.noreply.github.com>
2022-10-02 23:31:05 +1100

    aa

--- src/Bird.java
+++ src/Bird.java
@@ -1,8 +1,14 @@
 import java.awt.image.BufferedImage;
 
-public class Bird extends actorBot {
+public class Bird extends Actor implements actorBehaviour{
   public Bird(Cell loc, BufferedImage img, String desc, Player player) {
     super(loc, img, desc, player, 2);
     strat = new EscapeMove();
+  }
+
+  @Override
+  public void controlAble() {
+    // TODO Auto-generated method stub
+    
   }
 }
--- src/Dog.java
+++ src/Dog.java
@@ -1,6 +1,6 @@
 import java.awt.image.BufferedImage;
 
-public class Dog extends actorBot {
+public class Dog extends Actor {
   public Dog(Cell loc, BufferedImage img, String desc, Player player) {
     super(loc, img, desc, player, 1);
     strat = new ChaseMove();
